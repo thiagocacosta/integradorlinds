59c83b832b64ae8f92e992f5f165435ea92021e3
Lindomar <lindomarbatistao@gmail.com>
2024-11-28 14:17:03 -0300

    28/11/2024

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('produtos', views.listar_produtos),
-    
-]
--- data/localhost.py
+++
@@ -1,6 +0,0 @@
-import os
-
-caminho_atual = os.getcwd()
-c = caminho_atual.replace("\\", "/")
-
-print("Caminho da pasta atual:", c+"/")
